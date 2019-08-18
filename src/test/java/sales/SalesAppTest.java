dbd5270e2dcd389fb45d1c1fe09a2cea7c97f219
VincentHuang <vincent.huang@oocl.com>
2019-08-18 20:57:14 +0800

    [HGC]: add test function generateSalesActivityReport given not found sale then return null

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -2,13 +2,17 @@ package sales;
 
 import org.junit.Test;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
 public class SalesAppTest {
 
 	@Test
-	public void testGenerateReport() {
+	public void testGenerateReport_givenNotFoundSale_thenReturnNull() {
 		
-		SalesApp salesApp = new SalesApp();
-		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
-		
+		SalesApp salesApp = spy(new SalesApp());
+		doReturn(null).when(salesApp).getSales(any());
+		salesApp.generateSalesActivityReport("DUMMY", false);
+		verify(salesApp, times(1)).getSales(any());
 	}
 }
